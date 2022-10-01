cbade2b638b007e2a1206aef3f671f745bc98a70
Tarif Hossain <tarif@namespaceit.com>
2022-10-02 02:23:51 +0600

    add most conversion transaction raw query

--- backend/app/Http/Controllers/Api/HomeController.php
+++ backend/app/Http/Controllers/Api/HomeController.php
@@ -3,15 +3,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Wallet;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index(): JsonResponse {
-        $dd = Wallet::count('sender_id');
-        return response()->json($dd);
+        $most_conversion_transaction = DB::select( DB::raw(("SELECT COUNT(sender_id) as total, sender_id FROM wallets GROUP by sender_id ORDER by total DESC LIMIT 1;") ));
+        return response()->json($most_conversion_transaction);
     }
 }
